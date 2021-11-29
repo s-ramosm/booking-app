17eec69f2250c174624f3dfc192cc23aaecfc44f
Wilmer S. Ramos Madera <saircontactoramos@gmail.com>
2021-11-29 01:47:47 -0500

    Creatión of user model

---
+++ back/users/models.py
@@ -0,0 +1,30 @@
+"""Users models"""
+
+
+#Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+"user models"
+class users(AbstractUser):
+    
+    phone_number = models.CharField(max_length=20,blank=True)
+    
+    email = models.EmailField(blank=True)
+    
+    biography = models.CharField(max_length=200,blank=True)
+    
+    photo = models.ImageField(upload_to="user/pictures",blank=True, null = True)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) :
+        return self.username
+
+
+# Create your models here.
+
+
